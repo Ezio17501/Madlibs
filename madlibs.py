88e6e5c0fc5b8e8670ed1a419e066c1f11c3ea9b
Ezio17501 <kabilkumar17501@gmail.com>
2023-10-04 18:06:48 +0530

    Create madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,29 @@
+properNoun=input("Proper Noun:")
+Noun1=input("Noun:")
+Adjective1=input("Adjective:")
+Verb1=input("Verb:")
+Adjective2=input("Adjective:")
+Animal1=input("Animal:")
+Verb2=input("Verb:")
+Color1=input("Color:")
+Verb3=input("Verb ending in ing:")
+Adverb=input("Adverb ending in ly:")
+Number1=int(input("Number:"))
+measureOfTime=input("Measure of Time:")
+Color=input("Color:")
+Animal2=input("Animal:")
+Number2=int(input("Number:"))
+sillyWord=input("Silly Word:")
+Noun3=input("Noun:")
+
+
+
+madlib=f"This weekend I am going camping with {properNoun}. I packed my lantern, sleeping bag, and\
+{Noun1}. I am so {Adjective1} to {Verb1} in a tent. I am {Adjective2} we\
+might see a {Animal1}, they are kind of dangerous. We are going to hike, fish, and {Verb2}.\
+I have heard that the {Color1} lake is great for {Verb3}. Then we will\
+{Adverb}hike through the forest for {Number1} {measureOfTime}. If I see a\
+{Color} {Animal2} while hiking, I am going to bring it home as a pet! At night we will tell\
+{Number2} {sillyWord} stories and roast {Noun3} around the campfire!!"
+
+print(madlib)
