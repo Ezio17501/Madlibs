96071513269bc7d72afb9605a0e7fb5f87e2663a
Ezio17501 <kabilkumar17501@gmail.com>
2023-10-05 10:52:07 +0530

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -18,12 +18,12 @@ Noun3=input("Noun:")
 
 
 
-madlib=f"This weekend I am going camping with {properNoun}. I packed my lantern, sleeping bag, and\
-{Noun1}. I am so {Adjective1} to {Verb1} in a tent. I am {Adjective2} we\
-might see a {Animal1}, they are kind of dangerous. We are going to hike, fish, and {Verb2}.\
-I have heard that the {Color1} lake is great for {Verb3}. Then we will\
-{Adverb}hike through the forest for {Number1} {measureOfTime}. If I see a\
-{Color} {Animal2} while hiking, I am going to bring it home as a pet! At night we will tell\
+madlib=f"This weekend I am going camping with {properNoun}. I packed my lantern, sleeping bag, and \
+{Noun1}. I am so {Adjective1} to {Verb1} in a tent. I am {Adjective2} we \
+might see a {Animal1}, they are kind of dangerous. We are going to hike, fish, and {Verb2}. \
+I have heard that the {Color1} lake is great for {Verb3}. Then we will \
+{Adverb} hike through the forest for {Number1} {measureOfTime}. If I see a \
+{Color} {Animal2} while hiking, I am going to bring it home as a pet! At night we will tell \
 {Number2} {sillyWord} stories and roast {Noun3} around the campfire!!"
 
 print(madlib)
